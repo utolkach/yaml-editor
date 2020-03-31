3ffb949b67f95a3dc8b8af33f226f55399e260ee
Vladislav <vt486@yandex.by>
2020-03-31 20:20:19 +0300

    Initial commit

---
+++ SimpleYamlEditor/SimpleYamlEditor/MainWindow.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SimpleYamlEditor
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.grid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Location = new System.Drawing.Point(0, 41);
+            this.grid.Name = "grid";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.grid.Size = new System.Drawing.Size(1131, 737);
+            this.grid.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(9, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(94, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load configs";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(112, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save configs";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 778);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.grid);
+            this.Name = "MainWindow";
+            this.Text = "Simple YAML config editor";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SimpleYamlEditor/SimpleYamlEditor/MainWindow.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace SimpleYamlEditor
+{
+    public partial class MainWindow : Form
+    {
+
+        public static string DirectoryPath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // enabling Double buffering as said here: https://10tec.com/articles/why-datagridview-slow.aspx
+            if (!SystemInformation.TerminalServerSession)
+            {
+                var dgvType = grid.GetType();
+                var pi = dgvType.GetProperty("DoubleBuffered",
+                    BindingFlags.Instance | BindingFlags.NonPublic);
+                pi.SetValue(grid, true, null);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            grid.Rows.Clear();
+            grid.Columns.Clear();
+
+            var result = openFileDialog.ShowDialog(this);
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            var path = openFileDialog.FileName;
+            var directoryName = Path.GetDirectoryName(path);
+            DirectoryPath = directoryName;
+            var configs = LoadYaml(directoryName).OrderBy(x => x.Item1).ToList();
+            var keys = configs.SelectMany(x => x.Item2.Keys).Distinct().OrderBy(x => x).ToList();
+            grid.Columns.Add("ConfigName", "Config name");
+            configs.ForEach(x => grid.Columns.Add(x.Item1, x.Item1));
+            keys.ForEach(key =>
+            {
+                var values = configs
+                    .Select(env => env.Item2.TryGetValue(key, out var v) ? v as string : string.Empty)
+                    .ToArray();
+                var list = (new[] {key}).ToList();
+                list.AddRange(values);
+                grid.Rows.Add(list.ToArray());
+            });
+
+            grid.Columns[0].Frozen = true;
+            grid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.ColumnHeader);
+            grid.AutoResizeColumn(0, DataGridViewAutoSizeColumnMode.AllCells);
+            btnSave.Text = "Save configs";
+        }
+
+        private IEnumerable<(string, Dictionary<string, object>)> LoadYaml(string directoryName)
+        {
+            var deserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            foreach (var file in Directory.GetFiles(directoryName))
+            {
+                using (var sr = new StreamReader(file))
+                {
+                    object obj = null;
+                    try
+                    {
+                        obj = deserializer.Deserialize(new StringReader(sr.ReadToEnd()));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Can't parse:{file}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    yield return (Path.GetFileName(file),
+                        JsonHelper.DeserializeAndFlatten(JsonConvert.SerializeObject(obj)));
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            for (var c = 1; c < grid.ColumnCount; c++)
+            {
+                var filename = grid.Columns[c].HeaderText;
+                using (var sw = new StreamWriter(Path.Combine(DirectoryPath, filename), false))
+                {
+                    for (var r = 1; r < grid.RowCount; r++)
+                    {
+                        var value = grid[c, r].Value as string;
+                        var key = grid[0, r].Value as string;
+                        if (!string.IsNullOrEmpty(key))
+                            sw.WriteLine($"{key}: '{value}'");
+                    }
+                }
+
+            }
+
+            btnSave.Text = "Saved!";
+        }
+    }
+}
